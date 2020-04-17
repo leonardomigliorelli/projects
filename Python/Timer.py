46373483b80d9ccf546baf89d1585ca36453414f
nome-utente <la-propria-email>
2020-04-17 21:41:12 +0200

    Graphic times

---
+++ Python/Timer.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import time
+
+while True: # Inserire il tempo
+	try:
+		tempo=input("Inserisci il tempo (h:m:s)\n").replace(' ','').split(':')
+		if len(tempo) not in [1,2,3]:
+			raise ValueError
+
+		for i in range(3-len(tempo)):
+			tempo.append(0)
+
+		tempo=[int(tempo[i] or 0) for i in range(3)]
+
+		break
+	except ValueError:
+		print('Il valore inserito è errato, usare la formattazione corretta (ore:minuti:secondi)')
+		
+
+class Mainframe(tk.Frame): # Mainframe contains the widgets
+	
+	def __init__(self,master):
+
+		tk.Frame.__init__(self,master)
+
+		self.hour=tk.StringVar()
+		tk.Label(self, font=("Helvetica", 100), textvariable=self.hour).pack()
+
+		self.ore=tempo[0]
+		self.minuti=tempo[1]
+		self.secondi=tempo[2]+1
+
+		self.timeScaler()
+
+	def timeScaler(self):
+		if self.secondi==0:
+			self.secondi=59
+			if self.minuti==0:
+				self.minuti=59
+				if self.ore!=0:
+					self.ore-=1
+			else:
+				self.minuti-=1
+		else:
+			self.secondi-=1
+		
+		self.hour.set('{:02,}:{:02,}:{:02,}'.format(self.ore,self.minuti,self.secondi))
+		
+		if self.ore!=0 or self.minuti!=0 or self.secondi!=0:
+			self.after(1000, self.timeScaler)
+		else:
+			self.timeBlink()
+		
+	def timeBlink(self):
+		self.hour.set('    :    :    ')
+		self.after(1000, self.timeShow)
+
+	def timeShow(self):
+		self.hour.set('00:00:00')
+		self.after(1000, self.timeBlink)
+
+self=tk.Tk()
+self.title("Timer")
+self.geometry("400x100")
+
+Mainframe(self).pack()
+
+self.mainloop()
