2f959280e5b95a08260f46a04ee1cf15c170cd8a
nome-utente <la-propria-email>
2020-08-20 15:18:10 +0200

    improvements

--- Python/Numero di Grahan.py
+++ Python/Numero di Grahan.py
@@ -1,18 +1,20 @@
 import timeit
 
-tempoImpiegato=0
-valore=3
+tempoImpiegato = 0
+valore = 3
+
+
 def c():
 	global valore
-	valore**=3
+	valore **= 3
 
 
 for i in range(14):
 	print("Livello", i)
 
-	tempoFine=tempoImpiegato
-	tempoImpiegato=timeit.timeit(c, number=1)
-	diff=tempoImpiegato-tempoFine
+	tempoFine = tempoImpiegato
+	tempoImpiegato = timeit.timeit(c, number=1)
+	diff = tempoImpiegato-tempoFine
 
 	print("Ci ha impiegato {:.3} secondi con un incremento del {}%".format(tempoImpiegato, diff))
 
