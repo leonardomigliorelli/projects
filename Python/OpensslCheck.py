dde1d925823f2e12e5bbdaa3bd093bcbd7897ac9
leonardomigliorelli <leo.miglio@outlook.com>
2020-11-25 20:07:01 +0100

    update

--- Python/OpensslCheck.py
+++ Python/OpensslCheck.py
@@ -3,7 +3,6 @@ from hashlib import *
 possibilities = {'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512'}
 
 hash = None
-file = str(open(input("Inserire il percorso del file: ").strip(), 'r')).encode('utf-8')
 file = str(open(input("Inserire il percorso del file: ").strip(), 'rb')).encode('utf-8')
 knowHash = input("Inserire il codice da verificare (vuoto se non richiesto): ").strip()
 tipo = input("Inserire il tipo di hash: ").replace(' ', '').lower()
