0992e23f606eaf1abd62b14037e078a3676673b0
leonardomigliorelli <leo.miglio@outlook.com>
2021-01-26 17:49:07 +0100

    bug fixe

--- Python/createJSON.py
+++ Python/createJSON.py
@@ -1,6 +1,7 @@
 import json
 
 fileName = 'file.json'
+corrotto = False
 
 try:  # provo ad aprire il file
     file = open(fileName, 'r')
@@ -10,8 +11,9 @@ except FileNotFoundError:  # se non riesco lo creo
     myObj = {}
 except:
     print("ERRORE, FILE CORROTTO")
+    corrotto = True
 
-while True:
+while not corrotto:
     nome = input("Inserire il nome nel valore (! per uscire): ")
     if(nome == '!'):
         break
@@ -22,12 +24,13 @@ while True:
     # ["io"],3,'hello', 3.4
     valore = [var.replace('"', "'") for var in valore]
     valore = [var.strip('[]') for var in valore]
-    
+
     # TODO: trasforma var in valore
     for i in range(len(valore)):
         if(valore[i].replace("'", "") == valore[i]):  # se stiamo parlando di numeri
             # se il valore non ha uno 0 dopo la virgola è un intero
-            valore[i] = (int(valore[i]) if (str(float(valore[i])).split(".")[1] == '0') else float(valore[i]))
+            valore[i] = (int(valore[i]) if (
+                str(float(valore[i])).split(".")[1] == '0') else float(valore[i]))
         else:  # se stiamo parlando di stringhe
             valore[i] = valore[i].replace("'", "")
 
